4b6bb61c112b5eddfc770684c0a5e9aa0a1cf437
Kim YeongRae <dudfo3728@gmail.com>
2021-08-25 12:12:12 +0900

    Create Min함수.java
    구름 레벨1 MIn함수

---
+++ Min함수.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main
+{
+     public static void main(String[] args) throws Exception
+     {
+         Scanner sc = new Scanner(System.in);
+         System.out.println(Math.min(sc.nextInt(), sc.nextInt()));
+     }
+}
