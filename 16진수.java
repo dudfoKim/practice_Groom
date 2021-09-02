b5b6740a01226b879c57fc1e9c468b1bed10a5b7
Kim YeongRae <dudfo3728@gmail.com>
2021-09-02 09:53:30 +0900

    Create 16진수.java
    구름 레벨1 16진수

---
+++ 16진수.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main
+{
+	public static void main(String[] args) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		System.out.println(Integer.toHexString(a));
+	}
+}
